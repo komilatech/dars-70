4a08475756247ff3955d0fb57e6f3edf810630a9
komilatech <komilatech12@gmail.com>
2026-04-24 14:01:48 +0500

    app.py qo'shildi

---
+++ app.py
@@ -0,0 +1,8 @@
+
+def main():
+    text = "hTulogHP1fmvxSnQ5nYYi5DjxLV0Zr5hIb7kLHkru5XWfgD9Ihh4WsciJKycemb7Ug9V985104bOTn2wCkLdH71FFKNRoiBxNRPCc1thjWjxsk3yDNoIsV38M1vp9t7MQ8ObjulUxIgSKTaS9A7stK"
+    print("Dars 70")
+    print(text)
+
+if __name__ == "__main__":
+    main()
